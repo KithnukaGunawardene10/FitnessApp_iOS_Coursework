05f5b184b5c41c0f98fd543272adccd358bb3f96
KithnukaGunawardene <kithnukagunawardene10@gmail.com>
2023-04-20 10:30:45 +0530

    first commit

---
+++ fitnessappcoursework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitnessappcoursework
+//
+//  Created by K.M.Kithnuka Gunawardene on 2023-04-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
